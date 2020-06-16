509f5c94ff246fe1ad6641d8aa0970dc65a0c246
Cristian Bernal <cristianxsa15@gmail.com>
2020-06-16 17:12:38 +0200

    initial commit

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "12ae2b3301ca136091052e17bb7cf34b";
+
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_KEY,
+    },
+  });
+
+  return data;
+};
